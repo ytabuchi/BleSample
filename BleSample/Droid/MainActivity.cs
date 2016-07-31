7a47f0ac55a1aee54785999a17378558f6b0b864
Yoshito Tabuchi <ytabuchi@xlsoft.com>
2016-07-31 16:48:30 +0900

    imprement bluetooth adapter

--- BleSample/Droid/MainActivity.cs
+++ BleSample/Droid/MainActivity.cs
@@ -1,12 +1,22 @@
-﻿using Android.App;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Android.App;
+using Android.Content;
 using Android.Widget;
 using Android.OS;
+using Android.Bluetooth;
 
 namespace BleSample.Droid
 {
     [Activity(Label = "BleSample", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
+        // UUID of LBT-VRU01
+        const string uuid = "10B568BD-5CED-10E6-D467-EB3FBA189A95";
+
+        BluetoothAdapter mBluetoothAdapter;
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -14,8 +24,19 @@ namespace BleSample.Droid
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            var button = FindViewById<Button>(Resource.Id.myButton);
+            // Use this check to determine whether BLE is supported on the device.  Then you can
+            // selectively disable BLE-related features.
+            if (!PackageManager.HasSystemFeature(Android.Content.PM.PackageManager.FeatureBluetoothLe))
+            {
+                Toast.MakeText(this, Resource.String.ble_not_supported, ToastLength.Short).Show();
+                Finish();
+            }
 
+            // BluetoothManager
+            BluetoothManager manager = (BluetoothManager)GetSystemService(Context.BluetoothService); // using Android.Content;
+            mBluetoothAdapter = manager.Adapter;
+
+            var button = FindViewById<Button>(Resource.Id.myButton);
             button.Click += (sender, e) =>
             {
                 
--- BleSample/Droid/Resources/Resource.designer.cs
+++ BleSample/Droid/Resources/Resource.designer.cs
@@ -95,6 +95,9 @@ namespace BleSample.Droid
 			// aapt resource value: 0x7f040001
 			public const int app_name = 2130968577;
 			
+			// aapt resource value: 0x7f040002
+			public const int ble_not_supported = 2130968578;
+			
 			// aapt resource value: 0x7f040000
 			public const int hello = 2130968576;
 			
